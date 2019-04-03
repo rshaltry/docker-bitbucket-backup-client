d9057a4b88584280e77ce335d0bd969f6c9adf1c
Ryan Shaltry <rshaltry@shinola.com>
2019-04-03 18:27:02 -0400

    Updating bitbucket-backup script to set AWS_CONFIG_FILE env var

--- bitbucket-backup.sh
+++ bitbucket-backup.sh
@@ -2,18 +2,20 @@
 
 BACKUP_PATH=/backup/bitbucket    # Path to BitBucket backups
 DAYS_TO_KEEP=3                   # Days to keep old backups
-AWS_PROFILE=default              # AWS CLI profile name
-AWS_S3_BUCKET=mybucket-backups   # AWS S3 bucket name
-AWS_S3_DESTINATION=bitbucket     # Directory name for backup sync
+AWS_PROFILE=default                  # AWS CLI profile name
+AWS_S3_BUCKET=mybucket-backups       # AWS S3 bucket name
+AWS_S3_DESTINATION=bitbucket         # Directory name for backup sync
+AWS_CONFIG_FILE=/path/to/.aws/config # AWS configuration file path
 
 # Determine paths for script and executables
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DOCKER_COMPOSE=$(which docker-compose)
-FIND=$(which find)
-GZIP=$(which gzip)
-AWS=$(which aws)
+PATH=/bin:/usr/bin:/usr/local/bin
+DIR="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && /bin/pwd )"
+DOCKER_COMPOSE=docker-compose
+FIND=find
+GZIP=gzip
+AWS=aws
 
 $DOCKER_COMPOSE -f $DIR/docker-compose.yml run --rm bitbucket-backup \
   && $FIND $BACKUP_PATH -mtime +$DAYS_TO_KEEP -exec rm -f "{}" \; \
   && $GZIP $BACKUP_PATH/*.tar \
-  && $AWS s3 sync $BACKUP_PATH s3://$AWS_S3_BUCKET/$AWS_S3_DESTINATION --profile $AWS_PROFILE --delete
+  && $AWS s3 sync $BACKUP_PATH s3://$AWS_S3_BUCKET/$AWS_S3_DESTINATION --profile $AWS_PROFILE --delete --quiet
